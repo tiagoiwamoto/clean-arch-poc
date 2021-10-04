b82892e31f3b0a9ed924a07462fa533f659ede3b
Tiago Iwamoto <tiago.iwamoto@gmail.com>
2021-10-04 07:20:38 -0300

    Implementando repositório de usuário

--- src/main/java/br/com/tiagoiwamoto/cleanarchpoc/core/dataprovider/repository/UserRepository.java
+++ src/main/java/br/com/tiagoiwamoto/cleanarchpoc/core/dataprovider/repository/UserRepository.java
@@ -10,6 +10,8 @@ package br.com.tiagoiwamoto.cleanarchpoc.core.dataprovider.repository;
 
 import br.com.tiagoiwamoto.cleanarchpoc.core.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
